d3345bec1daab72f9f40cbf55c3a05c592e84190
JhonRocha <jhmrocha@gmail.com>
2020-02-09 15:00:11 -0300

    Updating

--- rectangles/target/debug/deps/rectangles-cb7e308c7625192f.d
+++
@@ -1,5 +0,0 @@
-/home/jh/jhworkspace/lean-rust/rectangles/target/debug/deps/rectangles-cb7e308c7625192f.rmeta: src/main.rs
-
-/home/jh/jhworkspace/lean-rust/rectangles/target/debug/deps/rectangles-cb7e308c7625192f.d: src/main.rs
-
-src/main.rs:
--- rectangles/target/debug/rectangles.d
+++
@@ -1,1 +0,0 @@
-/home/jh/jhworkspace/lean-rust/rectangles/target/debug/rectangles: /home/jh/jhworkspace/lean-rust/rectangles/src/main.rs
